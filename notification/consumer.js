9fe8ee2066481fb7b78e8d4603c96027ed7299da
Sandeep <sundeepp29@gmail.com>
2021-02-16 16:22:13 +0545

    dockerfile and docker compose file added

--- notification/consumer.js
+++ notification/consumer.js
@@ -3,7 +3,20 @@ const callTwilioApi = require("./twilio");
 
 module.exports = async () => {
   try {
-    const rabbitmqServer = "amqp://localhost:5672";
+    let rabbitmqServer
+    let retry = 10
+    while(retry >= 0){
+      try {
+        rabbitmqServer = "amqp://guest:guest@my-rabbit:5672";
+        break
+      } catch (err) {
+        console.log(err)
+        retry -= 1
+        console.log(`Retrying left ${retry}`)
+        await new Promise(resolve => setTimeout(resolve, 10000))
+      }
+    }
+    
     const connection = await amqp.connect(rabbitmqServer);
     const channel = await connection.createChannel();
     await channel.assertQueue("shopping");
